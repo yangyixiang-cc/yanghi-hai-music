de9e2dc382efc3172206aefbbaed9ee41580032e
yanghi <191894349@qq.com>
2023-03-28 22:05:08 +0800

    update .gitgonre

--- hai-music-server/src/main/java/com/yanghi/haimusic/service/UserDetailsService.java
+++
@@ -1,7 +0,0 @@
-package com.yanghi.haimusic.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.yanghi.haimusic.bean.UserDetails;
-
-public interface UserDetailsService extends IService<UserDetails> {
-}
